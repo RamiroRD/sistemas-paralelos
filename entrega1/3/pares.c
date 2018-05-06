92e65ef04cca0ba0f545910b12ad19e05d1a4f89
ramirord <ramiroromero94@gmail.com>
2018-05-06 15:11:01 -0300

    Punto 3 de entrega 1

---
+++ entrega1/3/pares.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <sys/time.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <stdint.h>
+
+int t;
+uint64_t n;
+int *V;
+
+double dwalltime()
+{
+	double sec;
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	sec = tv.tv_sec + tv.tv_usec / 1000000.0;
+	return sec;
+}
+
+int main(int argc, char **argv)
+{
+	/* Tiempos */
+	double ti, tf;
+
+	if (argc != 3) {
+		printf("pares T N\n");
+		return -1;
+	}
+
+	/* Cantidad de hilos */
+	t = atoi(argv[1]);
+	/* Dimensión del arreglo */
+	n = strtoll(argv[2], NULL, 10);
+	if (errno == ERANGE) {
+		printf("Fuera de rango.\n");
+		return -1;
+	}
+
+
+	printf("Alocando %f MiB...\n", n / 1048576.0);
+	V = malloc(sizeof(int) * n);
+	if (!V) {
+		perror("malloc");
+		return -1;
+	}
+
+
+	srand(time(NULL));
+	for (uint64_t i = 0; i < n; i++)
+		V[i] = rand();
+
+	omp_set_num_threads(t);
+
+	ti = dwalltime();
+
+	uint64_t pares = 0;
+#pragma omp parallel for reduction(+:pares)
+	for (uint64_t i = 0; i < n; i++)
+		if(V[i] % 2 == 0)
+			pares++;
+
+	tf = dwalltime();
+
+	printf("T = %f [s]\n", tf - ti);
+	printf("Cantidad de pares: %lu\n", pares);
+
+	free(V);
+
+	return 0;
+}
+
