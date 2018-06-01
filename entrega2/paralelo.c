c8deaa4be2226533f48888b5f7cd092647c8c805
matiglesias <matidxg@gmail.com>
2018-06-01 04:23:09 -0300

    FIN.

--- entrega2/paralelo.c
+++ entrega2/paralelo.c
@@ -27,7 +27,7 @@ int l_slice(int rank, int n, int t)
 	const int nt = n / t;
 	const int a = nt * (rank + 1);
 	const int b = nt * rank;
-	
+
 	return (a * (a + 1) - b * (b + 1)) >> 1;
 }
 
@@ -50,7 +50,7 @@ void add(double *C, const double *A, const double *B, int n, int t, int rank)
 {
 	const int slice = n * n / t;
 
-	for (int i = slice; i < (rank + 1) * slice; i++)
+	for (int i = 0; i < slice; i++)
 		C[i] = A[i] + B[i];
 }
 
@@ -63,7 +63,7 @@ void scale(double *A, double factor, int n, int t, int rank)
 {
 	const int slice = n * n / t;
 
-	for (int i = slice; i < (rank + 1) * slice; i++)
+	for (int i = 0; i < slice; i++)
 		A[i] *= factor;
 }
 
@@ -78,7 +78,7 @@ double sum(const double *A, int n, int t, int rank)
 
 	double s = 0;
 
-	for (int i = slice; i < (rank + 1) * slice; i++)
+	for (int i = 0; i < slice; i++)
 		s += A[i];
 
 	return s;
@@ -89,17 +89,17 @@ void multiply(double *C, const double *restrict A,
 	      const double *restrict B, int n, int t, int rank)
 {
 	const int slice = n / t;
-
+	const int offset = rank * slice;
 	double c;
 
 	/* Multiplicación convencional fila * columna */
-	for (int i = slice; i < (rank + 1) * slice; i++) {
+	for (int i = offset; i < (rank + 1) * slice; i++) {
 		for (int j = 0; j < n; j++) {
 			c = 0;
 			for (int k = 0; k < n; k++) {
-				c += A[i * n + k] * B[j * n + k];
+				c += A[(i - offset) * n + k] * B[j * n + k];
 			}
-			C[i * n + j] = c;
+			C[(i - offset) * n + j] = c;
 		}
 	}
 
@@ -116,19 +116,19 @@ double multiply_ll(double *restrict C, const double *restrict A,
 		 const double *restrict B, int n, int t, int rank)
 {
 	const int slice = n / t;
-
+	const int offset = rank * slice;
 	double c;
 	double s = 0;
 
-	for (int i = slice; i < (rank + 1) * slice; i++) {
+	for (int i = offset; i < (rank + 1) * slice; i++) {
 		for (int j = 0; j < n; j++) {
 			c = 0;
 			for (int k = 0; k <= i; k++)
-				c += A[L_FIL(i, k)] * B[j * n + k];
+				c += A[L_FIL(i - offset, k)] * B[j * n + k];
 
-			C[i * n + j] = c;
+			C[(i - offset) * n + j] = c;
 			if (i >= j)
-				s += A[L_FIL(i,j)];
+				s += A[L_FIL(i - offset,j)];
 		}
 	}
 	return s;
@@ -145,19 +145,19 @@ double multiply_ru(double *restrict C, const double *restrict A,
 		 const double *restrict B, int n, int t, int rank)
 {
 	const int slice = n / t;
-
+	const	int offset = rank * slice;
 	double c;
 	double s = 0;
 
-	for (int i = slice; i < (rank + 1) * slice; i++) {
+	for (int i = offset; i < (rank + 1) * slice; i++) {
 		for (int j = 0; j < n; j++) {
 			c = 0;
 			for (int k = 0; k <= j; k++)
-				c += A[i * n + k] * B[U_COL(k, j)];
+				c += A[(i - offset) * n + k] * B[U_COL(k, j)];
 
-			C[i * n + j] = c;
+			C[(i - offset) * n + j] = c;
 			if (i <= j)
-				s += B[U_COL(i, j)];
+				s += B[U_COL(i - offset, j)];
 		}
 	}
 
@@ -172,27 +172,27 @@ void wait_for_gdb()
 }
 
 /*
- * Compara las matrices A y B con una tolerancia epsilon. A y B deben estar 
+ * Compara las matrices A y B con una tolerancia epsilon. A y B deben estar
  * almacenadas de la misma forma.
  */
-double rms(double *A, double *B, int n)
+void error(double *A, double *B, int n, double *resultados)
 {
+	double sum = 0;
 	double res = 0;
 	for (int i = 0; i < n * n; i++) {
 		double d = (A[i] - B[i]);
 		d *= d;
 		res += d;
+		sum += B[i];
 	}
-
-	return res / (n * n);
+	resultados[0] = res / (n * n);
+	resultados[1] = sum / (n * n);
 }
 
-void load(const char *filename, double *A, double *B, double *C, double *D, 
+void load(const char *filename, double *A, double *B, double *C, double *D,
 		double *L, double *U, double *res, int n)
 {
 	int fd = open(filename, O_RDONLY);
-
-	res = malloc(sizeof(double) * n * n);
 
 	if (fd < 1) {
 		perror("load");
@@ -232,15 +232,15 @@ void common(int rank, int n, int t, double *A, double *B, double *C, double *D,
 		const int total = n * n;
 		const int slice = total / t;
 
-
 		/*
 		 * Distribución de datos
 		 */
-		MPI_Scatter(A, slice, MPI_DOUBLE, A + rank * slice, slice,
+
+		MPI_Scatter(A, slice, MPI_DOUBLE, A, slice,
 				MPI_DOUBLE, 0, MPI_COMM_WORLD);
 		MPI_Bcast(B, total, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 		MPI_Bcast(C, total, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		MPI_Scatter(D, slice, MPI_DOUBLE, D + rank * slice, slice,
+		MPI_Scatter(D, slice, MPI_DOUBLE, D, slice,
 				MPI_DOUBLE, 0, MPI_COMM_WORLD);
 		MPI_Bcast(U, n * (n + 1) >> 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 		/* El caso especial de L */
@@ -252,10 +252,11 @@ void common(int rank, int n, int t, double *A, double *B, double *C, double *D,
 				sendcounts[i] = L_FIL(n / t * (i + 1), 0) - displs[i];
 			}
 			MPI_Scatterv(L, sendcounts, displs, MPI_DOUBLE,
-				L + displs[rank], sendcounts[rank], MPI_DOUBLE,
+				L, sendcounts[rank], MPI_DOUBLE,
 				0, MPI_COMM_WORLD);
 		}
 
+		MPI_Barrier(MPI_COMM_WORLD);
 
 		/*
 		 * Cómputo
@@ -273,20 +274,23 @@ void common(int rank, int n, int t, double *A, double *B, double *C, double *D,
 		double *ABpLC = A;
 		add(ABpLC, AB, LC, n, t, rank);
 
-		double *res = ABpLC;
+		double *res = AB;
 
 		MPI_Allreduce(MPI_IN_PLACE, sums, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 		add(res, ABpLC, DU, n, t, rank);
 		scale(res, sums[0] * sums[1] / (elem * elem), n, t, rank);
 
+		MPI_Barrier(MPI_COMM_WORLD);
+
 		/*
 		 * Gather del resultado
 		 */
-		MPI_Gather(res + n * n / t * rank, n * n / t, MPI_DOUBLE, res, n * n / t,
+
+		MPI_Gather(res, slice, MPI_DOUBLE, res, slice,
 				MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
 		*result = res;
-	
+
 }
 
 INLINE
@@ -307,23 +311,30 @@ void master(const char *filename, int n, int t)
 		expected_result = malloc(n * n * sizeof(double));
 		load(filename, A, B, C, D, L, U, expected_result, n);
 	} else {
-		for (int i = 1; i < n * n; i++)
+		for (int i = 0; i < n * n; i++)
 			A[i] = 1;
 		memcpy(B, A, n * n * sizeof(double));
 		memcpy(C, A, n * n * sizeof(double));
 		memcpy(D, A, n * n * sizeof(double));
-		memcpy(U, A, n * n * sizeof(double));
-		memcpy(L, A, n * (n - 1) /2 * sizeof(double));
+		memcpy(U, A, n * (n + 1) /2 * sizeof(double));
+		memcpy(L, A, n * (n + 1) /2 * sizeof(double));
 	}
 
 	dur = dwalltime();
 
 	common(0, n, t, A, B, C, D, L, U, R1, &result);
 
-	printf("T = %f\n [s]", dwalltime() - dur);
+	printf("T = %f [s] \n", dwalltime() - dur);
 
 	if (filename)
-		fprintf(stderr, "RMS*RMS = %f\n", rms(expected_result, result, n));
+	{
+		double rms[2];
+		error(expected_result, result, n, rms);
+		fprintf(stderr, "RMS*RMS = %f\n", rms[0]);
+		fprintf(stderr, "promedio = %f\n", rms[1]);
+
+		free(expected_result);
+	}
 
 	free(A);
 	free(B);
@@ -346,8 +357,7 @@ void slave(int rank, int n, int t)
 	U = malloc(n * (n + 1) / 2 * sizeof(double));
 	R1 = malloc(n * n / t * sizeof(double));
 
-	const int offset = n * n / t * rank;
-	common(rank, n, t, A - offset, B, C, D - offset, L - L_FIL(n / t * rank, 0), U, R1 - offset, &result);
+	common(rank, n, t, A, B, C, D, L, U, R1, &result);
 
 	free(A);
 	free(B);
